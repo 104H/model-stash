0b0d49b78fa28fd6995c00f056729c316b4b1adf
Naeem Iqbal <naeem.iqbal@dfki.de>
2021-06-08 06:08:32 +0200

    complete functionality for one model

--- modelstash.sh
+++ modelstash.sh
@@ -32,28 +32,36 @@ modelfound=$?
 contains $platform ${platforms[@]}
 platformfound=$?
 
-if [ $datasetfound -eq 0 ] && [ $modelfound -eq 0 ] && [ $platformfound -eq 0 ]; then
+#if [ $datasetfound -eq 0 ] && [ $modelfound -eq 0 ] && [ $platformfound -eq 0 ]; then
+if [ $modelfound -eq 0 ] && [ $platformfound -eq 0 ]; then
     if [ "$model" == "yolov4" ]; then
         conda activate pytorch-YOLOv4
+        cd pytorch-YOLOv4
 
-        if [ $dataset == "mscoco2017val" ] && [ $platform == "pytorch" ]; then
-            cd pytorch-YOLOv4
-            python3 evaluate_on_coco.py
-        elif [ $dataset == "mscoco2017val" ] && [ $platform == "onnx" ]; then
-            cd pytorch-YOLOv4
-            python3 demo_darknet2onnx.py
+        if [ $platform == "pytorch" ]; then
+            # reads the model from scratch and then perofrms the inference once per image
+            for img in "$dataset"/*.jpg
+            do
+                python3 demo.py -imgfile "${img}"
+            done
+
+        elif [ $platform == "onnx" ]; then
+            python3 onnxinference.py yolov4_1_3_608_608_static.onnx "$dataset" 1
+
         else
             echo "Not Implemented"
         fi
-    if [ "$model" == "detectron2" ]; then
-        conda activate pytorch-deeplabxception-detectron2
 
-        if [ $dataset == "mscoco2017val" ] && [ $platform == "pytorch" ]; then
-            cd pytorch-YOLOv4
-            python3 evaluate_on_coco.py
-        elif [ $dataset == "mscoco2017val" ] && [ $platform == "onnx" ]; then
-            cd pytorch-YOLOv4
+    elif [ "$model" == "detectron2" ]; then
+        conda activate pytorch-deeplab-xception
+        cd pytorch-deeplab-xception
+
+        if [ $platform == "pytorch" ]; then
+            CUDA_VISIBLE_DEVICES=0 python3 train.py --backbone resnet --infer --workers 4 --epochs 1 --batch-size 1 --gpu-ids 0 --checkname deeplab-resnet --eval-interval 1 --dataset "$dataset"
+
+        elif [ $platform == "onnx" ]; then
             python3 demo_darknet2onnx.py
+
         else
             echo "Not Implemented"
         fi
